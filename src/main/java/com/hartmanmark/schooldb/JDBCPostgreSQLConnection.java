0be49b20782a97c3c2dd4fbd96ae88039ac9a043
Mark Hartman <whispergreencorvus@gmail.com>
2022-05-30 14:28:07 +0200

    .sql files

--- src/main/java/com/hartmanmark/schooldb/JDBCPostgreSQLConnection.java
+++ src/main/java/com/hartmanmark/schooldb/JDBCPostgreSQLConnection.java
@@ -1,8 +1,14 @@
 package com.hartmanmark.schooldb;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
 
 public class JDBCPostgreSQLConnection {
 
@@ -10,16 +16,41 @@ public class JDBCPostgreSQLConnection {
     private final String user = "postgres";
     private final String password = "123";
 
-    public Connection connect() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            if (conn == null) {
-                System.err.println("Failed to make connection");
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
+    public void connect() throws ClassNotFoundException, SQLException, FileNotFoundException {
+        Connection con = DriverManager.getConnection(url, user, password);      
+        ScriptRunner sr = new ScriptRunner(con);
+        Reader readerSchema = new BufferedReader(new FileReader("/home/user/java/GitLab/Task 7/Shema.sql"));
+        Reader readerData = new BufferedReader(new FileReader("/home/user/java/GitLab/Task 7/Data.sql"));
+        sr.runScript(readerSchema);
+        sr.runScript(readerData);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        Connection conn = null;
+//        try {
+//            conn = DriverManager.getConnection(url, user, password);
+//            if (conn == null) {
+//                System.err.println("Failed to make connection");
+//            }
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//        return conn;
     }
 }
--- src/main/java/com/hartmanmark/schooldb/Main.java
+++ src/main/java/com/hartmanmark/schooldb/Main.java
@@ -1,15 +1,14 @@
 package com.hartmanmark.schooldb;
 
+import java.io.FileNotFoundException;
 import java.sql.SQLException;
 
 public class Main {
 
-    public static void main(String[] args) throws SQLException {
-//        JDBCPostgreSQLConnection app = new JDBCPostgreSQLConnection();
-//        app.connect();
-        CreateTable createTable = new CreateTable();
-        createTable.createTable();
-
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException {
+        JDBCPostgreSQLConnection connection = new JDBCPostgreSQLConnection();
+        connection.connect();
+//        CreateTable createTable = new CreateTable();
+//        createTable.createTable();
     }
-
 }
